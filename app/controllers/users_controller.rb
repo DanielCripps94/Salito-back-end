71cd525cc50454899b34727b63222d61200a38bb
Amir Shariff <amirshariff@msn.com>
2020-03-18 00:11:43 +0000

    sign up backend route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,17 @@ class UsersController < ApplicationController
         end
     end
 
+    def create
+        user = User.new(name: params[:name],email:params[:email],password:params[:password])
+        if user.save
+            render json: {user: user, token: generate_token({id: user.id }) }
+        else
+            render json: { error: "You are not authorized" }
+        end
+    end
+
+    def generate_token(data)
+        JWT.encode(data, "my secret")
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post '/signin', to: 'users#sign_in'
+  post '/signup', to: 'users#create'
   get '/validate', to: 'users#validate'
 
 end
